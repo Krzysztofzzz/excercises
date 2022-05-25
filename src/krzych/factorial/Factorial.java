f94d1d4d82354e3aa548a9b776a7f37194c7fb43
Krzysztof <krzubrycki@gmail.com>
2022-05-25 22:31:25 +0200

    Factorial excercise recursive

--- src/krzych/factorial/Factorial.java
+++ src/krzych/factorial/Factorial.java
@@ -7,17 +7,24 @@ public class Factorial implements Runnable {
     public void run() {
         while (true) {
             int number = getNumber();
-            int factorial = factorialIterative(number);
+            //int factorial = factorialIterative(number);
+            int factorial = factorialRecursive(number);
             System.out.println("Factorial of " + number + " is " + factorial);
         }
     }
 
+    private int factorialRecursive(int number) {
+        if (number > 0)
+            return number * factorialRecursive(number - 1);
+        else return 1;
+    }
+
     private int factorialIterative(int number) {
         int factorial = 1;
-        do {
+        while (number > 0) {
             factorial *= number;
             number--;
-        } while (number > 0);
+        }
         return factorial;
     }
 
