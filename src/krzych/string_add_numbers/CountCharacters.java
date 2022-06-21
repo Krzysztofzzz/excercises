9cf67a96357481fe88edb2cd74b02b1942cacf50
Krzysztof <krzubrycki@gmail.com>
2022-06-21 19:22:53 +0200

    Added counting characers of given String exercise

---
+++ src/krzych/string_add_numbers/CountCharacters.java
@@ -0,0 +1,22 @@
+package krzych.string_add_numbers;
+
+public class CountCharacters {
+
+    public String count(String input) {
+        if (input == null || input.length() == 0)
+            return input;
+        char charToAdd = input.charAt(0);
+        String output = "";
+        int count = 1;
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) != charToAdd && i != input.length() - 1) {
+                output += String.valueOf(charToAdd) + count;
+                count = 1;
+                charToAdd = input.charAt(i);
+            } else
+                count++;
+        }
+        output += String.valueOf(charToAdd) + count;
+        return output;
+    }
+}
---
+++ test/java/krzych/string_add_numbers/CountCharactersTest.java
@@ -0,0 +1,46 @@
+package krzych.string_add_numbers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CountCharactersTest {
+    private CountCharacters countCharacters = new CountCharacters();
+
+    @Test
+    public void returnNullWhenStringIsNull() {
+        String input = null;
+        String output = countCharacters.count(input);
+        Assert.assertNull(output);
+    }
+
+    @Test
+    public void returnEmptyStringWhenInputIsEmpty() {
+        String input = "";
+        String output = countCharacters.count(input);
+        Assert.assertEquals(input, output);
+    }
+
+    @Test
+    public void whenOneCharacterGivenReturnSameCharacterPlusNumber() {
+        String input = "a";
+        String output = countCharacters.count(input);
+        String expected = "a1";
+        Assert.assertEquals(expected, output);
+    }
+
+    @Test
+    public void whenMultipleSameCharactersGivenReturnOneCharacterWithRepeatedNumber() {
+        String input = "aaaaaa";
+        String output = countCharacters.count(input);
+        String expected = "a6";
+        Assert.assertEquals(expected, output);
+    }
+
+    @Test
+    public void whenMultipleDifferentCharactersAreGivenReturnProperCount() {
+        String input = "aaaBBBdhhhhhh";
+        String output = countCharacters.count(input);
+        String expected = "a3B3d1h6";
+        Assert.assertEquals(expected, output);
+    }
+}
